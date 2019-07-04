c71b2075eda461d88a2d862b4801dc118cacc5c3
Erick Eduardo Baron <erickbaron123456@gmail.com>
2019-07-04 11:01:56 -0300

    Inserir completo, no repositorio

---
+++ Exercicio01-html/Repositorio/Conexao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class Conexao
+    {
+        public SqlCommand Conectar()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = @"";
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            return command;
+
+        }
+    }
+}
